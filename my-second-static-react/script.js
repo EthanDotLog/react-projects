69771644bbb182d4e4d67e59f03ff5a00750a952
EthanDotLog <148747739+EthanDotLog@users.noreply.github.com>
2024-04-19 21:42:43 -0500

    start

---
+++ my-second-static-react/script.js
@@ -0,0 +1,14 @@
+const nav = (
+  <div>
+    <h1>My Second Static React App</h1>
+    <p>this is the super cool nav bar</p>
+    <ul>
+      <li>One</li>
+      <li>Two</li>
+      <li>Three</li>
+      <li>Four - knock on a door or something</li>
+    </ul>
+  </div>
+)
+
+ReactDOM.render(nav, document.getElementById('root'))
