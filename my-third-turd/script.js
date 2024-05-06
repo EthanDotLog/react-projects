83aaba9513c23bae3e97e4fc070d937ad3a93bf5
EthanDotLog <148747739+EthanDotLog@users.noreply.github.com>
2024-05-06 01:05:02 -0500

    calm before the storm

--- my-third-turd/script.js
+++ my-third-turd/script.js
@@ -1,1 +1,14 @@
-ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById('root'))
+const navbar = (
+  <nav>
+    <h1>My First Static React App</h1>
+    <ul>
+      <li>One - this is a item</li>
+      <li>Two - we are both rendered in different ways</li>
+      <li>Three - check our code!</li>
+      <li>Four - knock on a door or something</li>
+    </ul>
+  </nav>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(navbar)
