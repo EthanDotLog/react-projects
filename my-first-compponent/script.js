4f89a1f566f0e193b3ba6e0b3ec078e668206d9c
EthanDotLog <148747739+EthanDotLog@users.noreply.github.com>
2024-05-06 22:55:29 -0500

    .

---
+++ my-first-compponent/script.js
@@ -0,0 +1,27 @@
+function ItsMe() {
+  return (
+    <div>
+      <header>
+        <h1>this is the header</h1>
+        <p>this is the subheader</p>
+      </header>
+      <main>
+        <h2>this is the main content</h2>
+        <p>this is the first paragraph</p>
+        <p>this is the second paragraph</p>
+        <p>this is the third paragraph</p>
+        <div style={{display: 'flex', justifyContent: 'space-evenly'}}> 
+          <p>this is the fourth paragraph</p>
+          <p>this is the fifth paragraph</p>
+          <p>this is the sixth paragraph</p>  
+        </div>
+      </main>
+      <footer>
+        <p>this is the footer</p>
+      </footer>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<ItsMe />, root)
